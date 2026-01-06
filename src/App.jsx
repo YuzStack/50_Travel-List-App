385a4884c8002441c18f827695d5e504eb2eca95
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-01-06 15:09:36 +0100

    Scaffold the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className='app'>
+      <Header />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className='list'>LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
